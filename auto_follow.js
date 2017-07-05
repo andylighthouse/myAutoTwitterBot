0c8f03fa1cfd00086888881dfd2b1689da06c4e1
Andy Li <andylibc@gmail.com>
2017-07-05 10:59:39 -0700

    changed auto follow file

--- auto_follow.js
+++ auto_follow.js
@@ -1,14 +1,16 @@
 var bot = require('./config')
+console.log("running...")
 
-//do not run for too long, will get banned from Twittier for making to many calls to the api
-var stream = bot.stream('user');
+//do not run for too long, will get banned from Twitter for making to many calls to the api
+var stream = bot.stream('statuses/filter', {track:'nba,trump,simpsons', language:'en'});
 stream.on('tweet', function(tweet){
- addFollower(tweet.user.screen_name);
+  addFollower(tweet.user.screen_name);
 });
 
 
 //look up relationship for a user, then follower that user if not followed already
 function addFollower(screen_name){
+  console.log('here')
   bot.get('friendships/lookup', {screen_name: screen_name}, function(err, data, response){
     if(err){
       console.log(err);
