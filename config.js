da9006d02089823e27d9a2f37c6986e179fb04c0
Andy Li <andylibc@gmail.com>
2017-07-06 14:35:33 -0700

    change env

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 var Twit = require('twit');
 
 var bot = new Twit({
-consumer_key: process.env.MYBOT_CONSUMER_KEY,
-consumer_secret: process.env.MYBOT_CONSUMER_SECRET,
-access_token: process.env.MYBOT_ACCESS_TOKEN,
-access_token_secret: process.env.MYBOT_ACCESS_TOKEN_SECRET,
+consumer_key: process.env.LOLOSBOT_CONSUMER_KEY,
+consumer_secret: process.env.LOLOSBOT_CONSUMER_SECRET,
+access_token: process.env.LOLOSBOT_ACCESS_TOKEN,
+access_token_secret: process.env.LOLOSBOT_ACCESS_TOKEN_SECRET,
 timeout_ms: 60000
 }) 
 
 module.exports = bot
+//MYBOT
