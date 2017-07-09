6ec6aa05cb36e80fbe8b7eaca6a8f8acc0108bfb
Andy Li <andylibc@gmail.com>
2017-07-09 15:45:36 -0700

    changed stream

--- index.js
+++ index.js
@@ -2,64 +2,29 @@ var bot = require('./config')
 console.log('bot starting....')
 
 //add id here of the people you want to retweet
-//var listOfId = ['755953153', '1094922224', '292116015', '19363044', '58601997', '599838934', '706915245575565300']
+var listOfId = ['50323173', '755953153', '1407822289', '58601997', '538547125', '37013920']
 
-
-var stream = bot.stream('statuses/filter', {follow: '755953153, 1094922224, 292116015, 19363044, 58601997, 599838934, 706915245575565300', language:'en'});
-  stream.on('tweet', function(tweet){
-    postRetweet(tweet.id_str);
-    addFollower(tweet.user.screen_name);
-  });
+var stream = bot.stream('user');
+stream.on('tweet', function(tweet){
+  var userId = tweet.user.id + ''
+  postRetweet(tweet.id_str, userId);
+});
 
 //retweet posts from stream
-function postRetweet(tweetId){
- bot.post('statuses/retweet/:id', {id: tweetId}, function(err, data, response){
-   if(err){
-     console.log(err);
-   }else{
-     console.log('Tweeted!');
-   }
- });
-}
-
-//look up relationship for a user, then follower that user if not followed already
-function addFollower(screen_name){
- bot.get('friendships/lookup', {screen_name: screen_name}, function(err, data, response){
-   if(err){
-     console.log(err);
-   }else{
-     if(data[0].connections[0] === 'none'){
-       bot.post('friendships/create', {screen_name: screen_name}, function(err, data, response){
-         if(err){
-           console.log(err);
-         }else{
-           console.log('Followed!');
-         }
-       });
-     }
-   }
- });
+function postRetweet(tweetId, userId){
+  if (listOfId.includes(userId)){
+    bot.post('statuses/retweet/:id', {id: tweetId}, function(err, data, response){
+      if(err){
+        console.log(err);
+      }else{
+        console.log('Tweeted!');
+      }
+    });
+  }
 }
 
 
-// var stream = bot.stream('user');
-// stream.on('tweet', function(tweet){
-//   var userId = tweet.user.id + ''
-//   postRetweet(tweet.id_str, userId);
-// });
 
-// //retweet posts from stream
-// function postRetweet(tweetId, userId){
-//   //if (listOfId.includes(userId)){
-//     bot.post('statuses/retweet/:id', {id: tweetId}, function(err, data, response){
-//       if(err){
-//         console.log(err);
-//       }else{
-//         console.log('Tweeted!');
-//       }
-//     });
-//   //}
-// }
 
 
 
