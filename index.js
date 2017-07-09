ce5702d4458de80390595562d546a7316854c00d
Andy Li <andylibc@gmail.com>
2017-07-09 15:54:08 -0700

    fixed userid

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ var bot = require('./config')
 console.log('bot starting....')
 
 //add id here of the people you want to retweet
-var listOfId = ['50323173', '755953153', '1407822289', '58601997', '538547125', '37013920']
+var listOfId = ['755953153', '1094922224', '292116015', '19363044', '58601997', '599838934', '706915245575565300']
 
-var stream = bot.stream('user');
-stream.on('tweet', function(tweet){
-  var userId = tweet.user.id + ''
-  postRetweet(tweet.id_str, userId);
-});
+
+
+var stream = bot.stream('statuses/filter', {follow: listOfId, language:'en'});
+  stream.on('tweet', function(tweet){
+    var userId = tweet.user.id + ''
+    postRetweet(tweet.id_str, userId);
+    addFollower(tweet.user.screen_name);
+  });
 
 //retweet posts from stream
 function postRetweet(tweetId, userId){
@@ -23,7 +26,24 @@ function postRetweet(tweetId, userId){
   }
 }
 
-
+//look up relationship for a user, then follower that user if not followed already
+function addFollower(screen_name){
+ bot.get('friendships/lookup', {screen_name: screen_name}, function(err, data, response){
+   if(err){
+     console.log(err);
+   }else{
+     if(data[0].connections[0] === 'none'){
+       bot.post('friendships/create', {screen_name: screen_name}, function(err, data, response){
+         if(err){
+           console.log(err);
+         }else{
+           console.log('Followed!');
+         }
+       });
+     }
+   }
+ });
+}
 
 
 
