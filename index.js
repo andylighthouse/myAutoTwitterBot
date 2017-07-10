bbe593ee922bcb33c3723e62052d0e6804ab89b5
Andy Li <andylibc@gmail.com>
2017-07-09 20:29:43 -0700

    too many followings

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ var bot = require('./config');
 console.log('bot starting....');
 
 //add id here of the people you want to retweet
-var listOfId = ['755953153', '1094922224', '292116015', '19363044', '58601997', '599838934']
-var idToFololow = listOfId.join()
+var listOfId = ['755953153', '1094922224', '292116015', '19363044', '58601997', '599838934'];
+var idToFololow = listOfId.join();
 
 var stream = bot.stream('statuses/filter', {follow: idToFololow});
 stream.on('tweet', function(tweet){
   postRetweet(tweet.id_str, tweet.user.id_str);
   var followerName = tweet.user.screen_name;
 
-  //try adding a new follower every 5 hours
-  setInterval(addFollower(followerName), 18000000);
+  //uncomment to add more followers
+  //addFollower(followerName);
 });
 
 //retweet posts from stream
@@ -31,21 +31,21 @@ function postRetweet(tweetId, userId){
 
 //look up relationship for a user, then follower that user if not followed already
 function addFollower(screen_name){
- bot.get('friendships/lookup', {screen_name: screen_name}, function(err, data, response){
-   if(err){
+  bot.get('friendships/lookup', {screen_name: screen_name}, function(err, data, response){
+    if(err){
      console.log(err);
-   }else{
-     if(data[0].connections[0] === 'none'){
-       bot.post('friendships/create', {screen_name: screen_name}, function(err, data, response){
-         if(err){
-           console.log(err);
-         }else{
-           console.log('Followed!');
-         }
-       });
-     }
-   }
- });
+    }else{
+      if(data[0].connections[0] === 'none'){
+        bot.post('friendships/create', {screen_name: screen_name}, function(err, data, response){
+          if(err){
+            console.log(err);
+          }else{
+            console.log('Followed!');
+          }
+        });
+      }
+    }
+  });
 }
 
 
